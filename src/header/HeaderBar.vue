9682921eefebce8009c1d9533a1886c71f22c48e
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2022-03-02 13:22:06 -0600

    Remove files from pre-built site template

---
+++ src/header/HeaderBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test-style">
+    Header Text
+  </div>
+</template>
+
+<style scoped>
+  .test-style {
+    background: black;
+    color: white;
+    border-radius: 0.2rem;
+    padding: 1rem;
+    font-family: Typewriter;
+  }
+</style>
